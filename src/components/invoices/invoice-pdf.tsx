e381c834cb04b54e25207e22e75df652d9980300
Miguel Silva <msilva@bidirco.com>
2024-09-05 09:36:49 +0100

    ...

--- src/components/invoices/invoice-pdf.tsx
+++ src/components/invoices/invoice-pdf.tsx
@@ -194,7 +194,7 @@ const InvoicePdf: FC<Props> = ({ invoice }) => {
 
               <SectionTitle> </SectionTitle>
               <SectionTitle>Validações</SectionTitle>
-              {/* <PDFDataBold>Validado por: {invoice.validacoes.nomePessoa}</PDFDataBold> */}
+              <PDFDataBold>Validado por: {invoice.validacoes.nomePessoa}</PDFDataBold>
               <br></br>
               {/* <PDFDataBold>Em: {invoice.validacoes.validadoEm}</PDFDataBold> */}
               <br></br>
