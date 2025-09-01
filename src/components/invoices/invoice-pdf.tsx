e17a9e8e28acae9b2263ff38a5e3d64ac042976b
PauloTeixeiraService <163354199+PauloTeixeiraService@users.noreply.github.com>
2025-09-01 21:55:17 +0200

    Update invoice-pdf.tsx

--- src/components/invoices/invoice-pdf.tsx
+++ src/components/invoices/invoice-pdf.tsx
@@ -91,6 +91,18 @@ const baseUrlFont = true
  * Main Invoice Component.
  */
 const InvoicePdf: FC<Props> = ({ invoice }) => {
+
+  const getCurrentTimestamp = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero-indexed
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+  };
   
   const subTotal = useMemo(() => {
     let subTotal = 0;
@@ -130,7 +142,7 @@ const InvoicePdf: FC<Props> = ({ invoice }) => {
           <InvoiceTitle title="RELATÓRIO" />
           <SectionTitle> </SectionTitle>
           
-          <InvoiceInfo invoiceNumber={localStorage.getItem("count")} />
+          <InvoiceInfo invoiceNumber={getCurrentTimestamp()} />
               <SectionTitle> </SectionTitle>
               <SectionTitle> </SectionTitle>
           <br></br>
