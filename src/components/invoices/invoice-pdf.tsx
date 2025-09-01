e5575d478dac2fde9debdad5c7ae90791b0da373
PauloTeixeiraService <163354199+PauloTeixeiraService@users.noreply.github.com>
2025-09-01 21:32:05 +0200

    Update invoice-pdf.tsx

--- src/components/invoices/invoice-pdf.tsx
+++ src/components/invoices/invoice-pdf.tsx
@@ -87,22 +87,23 @@ const baseUrlFont = true
 //   ],
 // });
 
-function getCurrentTimestamp() {
-    const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero indexed
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
-    const seconds = String(now.getSeconds()).padStart(2, '0');
-    
-    return `${year}${month}${day}${hours}${minutes}${seconds}`;
-}
-
 /**
  * Main Invoice Component.
  */
 const InvoicePdf: FC<Props> = ({ invoice }) => {
+
+   const getCurrentTimestamp = () => {
+      const now = new Date();
+      const year = now.getFullYear();
+      const month = String(now.getMonth() + 1).padStart(2, '0');
+      const day = String(now.getDate()).padStart(2, '0');
+      const hours = String(now.getHours()).padStart(2, '0');
+      const minutes = String(now.getMinutes()).padStart(2, '0');
+      const seconds = String(now.getSeconds()).padStart(2, '0');
+      
+      return `${year}${month}${day}${hours}${minutes}${seconds}`;
+  };
+  
   const subTotal = useMemo(() => {
     let subTotal = 0;
     
