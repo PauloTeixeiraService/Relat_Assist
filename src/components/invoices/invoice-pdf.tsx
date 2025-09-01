32f2cb7987f5de78d25c052f2f93f56f22ad7094
PauloTeixeiraService <163354199+PauloTeixeiraService@users.noreply.github.com>
2025-09-01 21:26:13 +0200

    Update invoice-pdf.tsx

--- src/components/invoices/invoice-pdf.tsx
+++ src/components/invoices/invoice-pdf.tsx
@@ -87,6 +87,18 @@ const baseUrlFont = true
 //   ],
 // });
 
+function getCurrentTimestamp() {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, '0'); // Months are zero indexed
+    const day = String(now.getDate()).padStart(2, '0');
+    const hours = String(now.getHours()).padStart(2, '0');
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    
+    return `${year}${month}${day}${hours}${minutes}${seconds}`;
+}
+
 /**
  * Main Invoice Component.
  */
@@ -131,7 +143,7 @@ const InvoicePdf: FC<Props> = ({ invoice }) => {
           
           {/* <InvoiceInfo invoiceNumber={localStorage.getItem("count")} />*/}
           
-          <InvoiceInfo invoiceNumber='' date={invoice.date} />
+          <InvoiceInfo invoiceNumber={getCurrentTimestamp()} />
               <SectionTitle> </SectionTitle>
               <SectionTitle> </SectionTitle>
           <br></br>
